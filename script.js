a8d59af62b44be7dd3f4daba13098aac797f9f56
Freshynex <bence.recsnik@gmail.com>
2022-10-10 21:28:30 +0200

    Added function to add btn and calculate btn (bottom left)

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function operate(operator,a,b){
     let divide = 0;	//divide 4
     let modulo = 0;	//modulo 5
     
-    let number1 = a;
-    let number2 = b;
+
     
     if(operator === undefined){ //
        add = 0;		
@@ -75,16 +74,68 @@ function operate(operator,a,b){
     return result;
   
   }
-
+  let operation = '';
+  let number1 = null;
+  let number2 = null;
 
   //----------CALCULATOR BUTTONS--------
-
+  let displayValue;
+  let calcDisplay = document.querySelector('.displayfield');
+  //calcDisplay.textContent = displayValue;
   let key1 = document.querySelectorAll('.calcbtn');
   Array.from(key1);
   key1.forEach(button => {
     button.addEventListener('click', (e) => {
     let buttonPressed = e.target.classList.item(1);
+    let buttonPressedValue = parseInt(e.target.textContent);
+    //console.log(buttonPressedValue + 'value of button');
     console.log(buttonPressed);
+    if(!isNaN(buttonPressedValue)){
+      if(number2 !== null){
+        number1 = number2;
+        number2 = null;
+      }    
+      calcDisplay.textContent += buttonPressedValue;
+      displayValue = parseInt(calcDisplay.textContent);
+      console.log(displayValue);
+      console.log(typeof displayValue); 
+      
+    }
+  
+    else{
+      
+      switch(buttonPressed){
+        case '+btn':
+         if(number1 === null){
+          operation = "add";
+          number1 = displayValue;
+          displayValue = null;
+          calcDisplay.textContent = '';
+          console.log(number1);
+         }
+         else{
+          operation = "add";
+          displayValue = null;
+          calcDisplay.textContent = '';
+         }
+         break;
+         case 'eqlbtn':
+          console.log(number1);
+          if(number1 !== null){
+            number2 = displayValue;
+            result = number1 + number2;
+            number1 = result;
+            
+            calcDisplay.textContent = result;
+            number2 = null;
+            result = null;
+          }
+        break;
+         
+         
+
+      }
+    }
     //checktarget(buttonPressed);
     }
   )
